d6a40255b8e1366464a980707159e524c8491a9f
Roghashin Timbiti <rtimbiti@gmail.com>
2020-02-18 10:34:06 +0300

    Add Fonts

--- App.js
+++ App.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
+import * as Font from 'expo-font'
+import { AppLoading } from 'expo'
+
 import App from './src/navigation'
 
-export default () => (
-  <App />
-);
+const fetchFonts = async () => {
+  await Font.loadAsync({
+    "amsterdam": require("./assets/fonts/RainbowBridgePersonalUse-ERae.ttf")
+  })
+}
+
+export default class MyProgram extends Component {
+
+  state = {
+    isFontLoaded: false
+  }
+
+  async componentDidMount() {
+    console.log('componentDidMount')
+    await Font.loadAsync({
+      "amsterdam": require("./assets/fonts/BeautyMountains.ttf")
+    })
+    this.setState({ isFontLoaded: true })
+  }
+
+  render() {
+    if (!this.state.isFontLoaded) {
+      return (
+        <AppLoading />
+      )
+    }
+    return (
+      <App />
+    )
+  }
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Tone it up Clone
+
+This is a clone for the [Tone it up](https://apps.apple.com/us/app/tone-it-up-workout-fitness/id1252077383). No Images where available, I used plain background colors.
+
+# [Demo](https://www.loom.com/share/486249d30bfe4f0cb60a5bcac1f2e348)
